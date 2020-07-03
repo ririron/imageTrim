c5520f88f29e2cc58736a344faea28b1ea502ff3
ririron <townageriron@gmail.com>
2020-07-03 12:00:37 +0900

    add trim_cv2image

---
+++ trim_cv2image.py
@@ -0,0 +1,17 @@
+import os
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+path = './../../resource/manualTrim'  #input("input path:")
+target = 'Proc201406250013_1_58_trimV.jpg'  #input("targetIMG:")
+print("cutintg [" + target + "]")
+name = input("input cutting image name:")
+img = cv2.imread(path + '/' + target)
+
+x_ul, x_dr = map(int, input("input X [upLeft downRight]:").split())
+y_ul, y_dr = map(int, input("input Y [upLeft downRight]:").split())
+
+cut_img = img[y_ul:y_dr, x_ul:x_dr]
+
+cv2.imwrite(path + '/' + name + target, cut_img)

d23cdfa5c05678ef50a2a3d273cd4987f8679541
ririron <townageriron@gmail.com>
2020-07-14 16:43:56 +0900

    modify

--- trim_cv2image.py
+++ trim_cv2image.py
@@ -4,7 +4,7 @@ import cv2
 import matplotlib.pyplot as plt
 
 path = './../../resource/manualTrim'  #input("input path:")
-target = 'CNV2000001168362_1_8_trimV.jpg'  #input("targetIMG:")
+target = input("targetIMG:") #'CNV2000002057140_1_13_trimV.jpg' 
 print("cutintg [" + target + "]")
 name = input("input cutting image name:")
 img = cv2.imread(path + '/' + target)
